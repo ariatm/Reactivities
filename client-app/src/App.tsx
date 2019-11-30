40c1029acb9205621f58539f92fde192d40a3748
Ariatm <aria_tm@yahoo.com>
2019-11-30 16:41:23 +0330

    End of Section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,21 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { Header, Icon, List } from "semantic-ui-react";
-import './App.css';
-import axios from 'axios';
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   state = {
     values: []
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-      .then((response) => {
-        // console.log(response);
-        this.setState({
-          values: response.data
-        })     
-      })
+    axios.get("http://localhost:5000/api/values").then(response => {
+      // console.log(response);
+      this.setState({
+        values: response.data
+      });
+    });
   }
 
   render() {
@@ -31,7 +30,6 @@ class App extends Component {
             <List.Item key={value.id}>{value.name}</List.Item>
           ))}
         </List>
-
       </div>
     );
   }
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #install NVM-Windows
-# chose desired node version
+# choose desired node version
 $version = "8.12.0"
 # install nvm w/ cmder
 choco install cmder
@@ -15,8 +15,7 @@ nvm use $version
 #installing react-app
 npx create-react-app client-app --use-npm --typescript
 
-
-#not good enough as a react tool
+#react dev tool
 React Developer Tools for chrome/firefox by facebook (v4+)
 
 #install axios as http-client inside "client-app"
@@ -25,5 +24,5 @@ npm install axios
 #install Semantic UI React inside "client-app"
 npm install semantic-ui-react
 
-#add link to Semantic UI React CSS
+#add Semantic UI React CSS link to public\index.html
 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
